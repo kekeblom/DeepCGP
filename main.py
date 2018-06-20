d541ec0cdd92e70041b5018dfe052bb866965b3e
Kenneth Blomqvist <kekeblom@gmail.com>
2018-06-20 13:59:14 +0300

    Decay learning rate

--- main.py
+++ main.py
@@ -8,8 +8,45 @@ from kernels import ConvKernel, PatchInducingFeature
 class MNIST(object):
     def __init__(self):
         self._load_data()
+        self._setup_model()
+        self._setup_optimizer()
+        self.epoch = 0
+
+    def train_epoch(self):
+        self._step()
+        self._optimize()
+        self._evaluate()
+
+    def _optimize(self):
+        maxiter = self.X_train.shape[0] // self.minibatch_size
+        self.optimizer.minimize(self.model, maxiter=maxiter, global_step=self.global_step)
+        self._print_status_line()
+
+    def _step(self):
+        self.epoch += 1
+
+    def _print_status_line(self):
+        sess = self.model.enquire_session()
+        objective = sess.run(self.model.objective)
+        print("Epoch: {}, objective: {}".format(self.epoch, objective))
+        lr = self.model.enquire_session().run(self.lr_schedule)
+        print('lr: {}'.format(lr))
+
+    def _evaluate(self):
+        correct = 0
+        batch_size = 512
+        for i in range(len(self.Y_test) // batch_size + 1):
+            the_slice = slice(i * batch_size, (i+1) * batch_size)
+            X = self.X_test[the_slice]
+            Y = self.Y_test[the_slice]
+            probabilities, _ = self.model.predict_y(X)
+            predicted_class = probabilities.argmax(axis=1)
+            correct += (predicted_class == Y).sum()
+        print("Accuracy: {}".format(correct / self.Y_test.size))
+
+    def _setup_model(self):
         self.num_inducing = 750
-        window_size = 3
+        window_size = 5
         patch_size = window_size**2
         kernel = ConvKernel(
                 gpflow.kernels.RBF(input_dim=patch_size),
@@ -26,32 +63,18 @@ class MNIST(object):
                 feat=inducing_features,
                 num_latent=10,
                 likelihood=gpflow.likelihoods.MultiClass(10),
-                whiten=True,
+                whiten=False,
                 minibatch_size=self.minibatch_size)
         self.model.compile()
 
-        self.optimizer = gpflow.train.AdamOptimizer(learning_rate=0.01)
-
-    def run(self):
-        self._optimize()
-        self._evaluate()
-
-    def _optimize(self):
-        maxiter = self.X_train.shape[0] // self.minibatch_size
-        self.optimizer.minimize(self.model, maxiter=maxiter)
-        print(self.model)
-
-    def _evaluate(self):
-        correct = 0
-        batch_size = 512
-        for i in range(len(self.Y_test) // batch_size):
-            the_slice = slice(i * batch_size, (i+1) * batch_size)
-            X = self.X_test[the_slice]
-            Y = self.Y_test[the_slice]
-            probabilities, _ = self.model.predict_y(X)
-            predicted_class = probabilities.argmax(axis=1)
-            correct += (predicted_class == Y).sum()
-        print("Accuracy: {}".format(correct / self.Y_test.size))
+    def _setup_optimizer(self):
+        self.global_step = tf.train.get_or_create_global_step()
+        self.model.enquire_session().run(self.global_step.initializer)
+        decay_every = 10
+        self.lr_schedule = tf.train.exponential_decay(1e-2, self.global_step,
+                decay_steps=len(self.X_train) // self.minibatch_size * decay_every,
+                decay_rate=0.1)
+        self.optimizer = gpflow.train.AdamOptimizer(learning_rate=self.lr_schedule)
 
     def _load_data(self):
         (self.X_train, self.Y_train), (
@@ -66,6 +89,6 @@ class MNIST(object):
 if __name__ == "__main__":
     experiment = MNIST()
     while True:
-        experiment.run()
+        experiment.train_epoch()
 
 
