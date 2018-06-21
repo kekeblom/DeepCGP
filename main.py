d28ba1da28be591aebf1b6d69a4453b53465c4c6
Kenneth Blomqvist <kekeblom@gmail.com>
2018-06-21 12:07:23 +0300

    Logging

--- main.py
+++ main.py
@@ -1,51 +1,38 @@
+import math
 import numpy as np
 import tensorflow as tf
 import gpflow
 import observations
+import utils
 from gpflow import settings
 from kernels import ConvKernel, PatchInducingFeature
 
 class MNIST(object):
-    def __init__(self):
+    def __init__(self, flags):
+        self.flags = flags
+
         self._load_data()
         self._setup_model()
         self._setup_optimizer()
-        self.epoch = 0
+        self._setup_logger()
 
-    def train_epoch(self):
-        self._step()
+    def close(self):
+        self.log.close()
+
+    def train_step(self):
         self._optimize()
-        self._evaluate()
+        self._log_step()
+
+    def _log_step(self):
+        entry = self.log.write_entry(self.model)
+        print(entry)
 
     def _optimize(self):
-        maxiter = self.X_train.shape[0] // self.minibatch_size
-        self.optimizer.minimize(self.model, maxiter=maxiter, global_step=self.global_step)
-        self._print_status_line()
-
-    def _step(self):
-        self.epoch += 1
-
-    def _print_status_line(self):
-        sess = self.model.enquire_session()
-        objective = sess.run(self.model.objective)
-        print("Epoch: {}, objective: {}".format(self.epoch, objective))
-        lr = self.model.enquire_session().run(self.lr_schedule)
-        print('lr: {}'.format(lr))
-
-    def _evaluate(self):
-        correct = 0
-        batch_size = 512
-        for i in range(len(self.Y_test) // batch_size + 1):
-            the_slice = slice(i * batch_size, (i+1) * batch_size)
-            X = self.X_test[the_slice]
-            Y = self.Y_test[the_slice]
-            probabilities, _ = self.model.predict_y(X)
-            predicted_class = probabilities.argmax(axis=1)
-            correct += (predicted_class == Y).sum()
-        print("Accuracy: {}".format(correct / self.Y_test.size))
+        numiter = self.flags.test_every
+        self.optimizer.minimize(self.model, maxiter=numiter, global_step=self.global_step)
 
     def _setup_model(self):
-        self.num_inducing = 750
+        num_inducing = self.flags.M
         filter_size = 5
         patch_size = filter_size**2
         kernel = ConvKernel(
@@ -54,14 +41,13 @@ class MNIST(object):
                 filter_size=filter_size,
                 stride=1,
                 channels=1)
-        Z = kernel.init_inducing_patches(self.X_train, self.num_inducing)
+        Z = kernel.init_inducing_patches(self.X_train, num_inducing)
         inducing_features = PatchInducingFeature(Z)
         print("z initialized")
         self.minibatch_size = 128
         self.model = gpflow.models.SVGP(self.X_train, self.Y_train,
                 kern=kernel,
                 feat=inducing_features,
-                num_latent=10,
                 likelihood=gpflow.likelihoods.MultiClass(10),
                 whiten=False,
                 minibatch_size=self.minibatch_size)
@@ -70,25 +56,84 @@ class MNIST(object):
     def _setup_optimizer(self):
         self.global_step = tf.train.get_or_create_global_step()
         self.model.enquire_session().run(self.global_step.initializer)
-        decay_every = 10
         self.lr_schedule = tf.train.exponential_decay(1e-2, self.global_step,
-                decay_steps=len(self.X_train) // self.minibatch_size * decay_every,
+                decay_steps=self.flags.lr_decay_steps,
                 decay_rate=0.1)
         self.optimizer = gpflow.train.AdamOptimizer(learning_rate=self.lr_schedule)
 
     def _load_data(self):
+        if self.flags.fashion:
+            load_fn = observations.fashion_mnist
+        else:
+            load_fn = observations.mnist
         (self.X_train, self.Y_train), (
-                self.X_test, self.Y_test) = observations.mnist('/tmp/mnist/')
+                self.X_test, self.Y_test) = load_fn('/tmp/mnist/')
+        self.Y_train = self.Y_train.reshape(-1, 1)
+        self.Y_test = self.Y_test.reshape(-1, 1)
+        self._select_training_points()
         self.X_train = self._preprocess(self.X_train)
         self.X_test = self._preprocess(self.X_test)
+        self.Y_train, self.Y_test
+
+    def _select_training_points(self):
+        chosen = slice(0, self.flags.N)
+        self.X_train = self.X_train[chosen, :]
+        self.Y_train = self.Y_train[chosen, :]
 
     def _preprocess(self, data):
         return (data / 255.0).astype(settings.float_type)
 
+    def _setup_logger(self):
+        loggers = [
+            utils.GlobalStepLogger(),
+            utils.LearningRateLogger(self.lr_schedule),
+            utils.AccuracyLogger(self.X_test, self.Y_test),
+            utils.LogLikelihoodLogger()
+        ]
+        self.log = utils.Log(self.flags.log_dir, self.flags.name, loggers)
+        self.log.write_flags(self.flags)
+
+
+def read_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fashion', action='store_true', default=False,
+            help="Use fashion MNIST instead of regular MNIST.")
+    parser.add_argument('-N', type=int,
+            help="How many training examples to use.", default=60000)
+    parser.add_argument('-M', type=int,
+            help="How many inducing points to use.")
+    parser.add_argument('--name', type=str, required=True,
+            help="What to call the experiment. Determines the directory where results are dumped.")
+    parser.add_argument('--lr-decay-steps', type=int, default=100000,
+            help="The program uses exponential learning rate decay with 0.1 decay every lr-decay-steps.")
+    parser.add_argument('--test-every', type=int, default=10000,
+            help="How often to evaluate the test accuracy. Unit optimization iterations.")
+    parser.add_argument('--log-dir', type=str, default='results',
+            help='Directory to write the results to.')
+    return parser.parse_args()
+
+def train_steps(flags):
+    # Roughly until the learning rate becomes 1e-5
+    minibatch_size = 128
+    steps_in_epoch = flags.N / minibatch_size
+
+    return math.ceil(flags.lr_decay_steps * 3 / flags.test_every)
+
+def main():
+    np.random.seed(5033)
+
+    flags = read_args()
+
+    experiment = MNIST(flags)
+
+    try:
+        for i in range(train_steps(flags)):
+            experiment.train_step()
+    finally:
+        experiment.close()
 
 if __name__ == "__main__":
-    experiment = MNIST()
-    while True:
-        experiment.train_epoch()
+    main()
 
 
---
+++ utils.py
@@ -0,0 +1,134 @@
+import csv
+import math
+import os
+import gpflow
+import toml
+import tensorflow as tf
+
+def ensure_dir(path):
+    if not os.path.exists(path):
+        all_but_last = os.path.split(path)[0:-1]
+        all_but_last = [p for p in all_but_last if p]
+        if len(all_but_last) > 0:
+            ensure_dir(os.path.join(*all_but_last))
+        os.mkdir(path)
+
+class Logger(object):
+    """Logger is an abstract base class for loggers.
+    Loggers can be passed to the log. At each log write each logger will be called
+    and the output will be logged.
+
+    The property title should be set by subclasses. It will determine the column title.
+    """
+
+    def __call__(self, model):
+        """implemented by subclass. Should return whatever wants to be logged."""
+        raise NotImplementedError()
+
+class GlobalStepLogger(Logger):
+    def __init__(self):
+        self.title = "global_step"
+
+    def __call__(self, model):
+        sess = model.enquire_session()
+        global_step = tf.train.get_or_create_global_step()
+        return sess.run(global_step)
+
+class LearningRateLogger(Logger):
+    def __init__(self, learning_rate_op):
+        self.title = "lr"
+        self.learning_rate_op = learning_rate_op
+
+    def __call__(self, model):
+        sess = model.enquire_session()
+        return sess.run(self.learning_rate_op)
+
+class AccuracyLogger(Logger):
+    def __init__(self, X_test, Y_test):
+        self.title = 'test_accuracy'
+        self.X_test, self.Y_test = X_test, Y_test
+
+    def __call__(self, model):
+        correct = 0
+        batch_size = 512
+        for i in range(len(self.Y_test) // batch_size + 1):
+            the_slice = slice(i * batch_size, (i+1) * batch_size)
+            X = self.X_test[the_slice]
+            Y = self.Y_test[the_slice]
+            probabilities, _ = model.predict_y(X)
+            predicted_class = probabilities.argmax(axis=1)
+            correct += (predicted_class == Y).sum()
+        return correct / self.Y_test.size
+
+class LogLikelihoodLogger(Logger):
+    def __init__(self):
+        self.title = 'train_log_likelihood'
+        self.batch_size = 512
+
+    def __call__(self, model):
+        with gpflow.decors.params_as_tensors_for(model):
+            X_holder, Y_holder = model.X, model.Y
+        log_likelihood = 0.0
+        batches = math.ceil(model.X._value.shape[0] / self.batch_size)
+        for i in range(batches):
+            the_slice = slice(i * self.batch_size, (i+1) * self.batch_size)
+            X = model.X._value[the_slice]
+            Y = model.Y._value[the_slice]
+            batch_likelihood = model.compute_log_likelihood(feed_dict={
+                X_holder: X,
+                Y_holder: Y
+                })
+            log_likelihood += batch_likelihood
+        return log_likelihood
+
+class InducingPointSaver(object):
+    def __init__(self, model):
+        self.model = model
+
+    def write(self):
+        import ipdb; ipdb.set_trace()
+
+class Log(object):
+    def __init__(self, log_dir, run_name, loggers):
+        self.loggers = loggers
+        self.result_dir = self._result_dir(log_dir, run_name)
+        self._start_log_file(run_name)
+        self._write_headers()
+        self.entries = 0
+
+    def _result_dir(self, log_dir, run_name):
+        path = os.path.join(log_dir, run_name)
+        ensure_dir(path)
+        return path
+
+    def _write_headers(self):
+        self.headers = ["Entry"] + [l.title for l in self.loggers]
+        self.csv_writer.writerow(self.headers)
+
+    def _start_log_file(self, name):
+        file_path = os.path.join(self.result_dir, name + '.csv')
+        self.file = open(file_path, 'wt')
+        self.csv_writer = csv.writer(self.file)
+
+    def write_entry(self, model):
+        entry = [self.entries] + [logger(model) for logger in self.loggers]
+        self.csv_writer.writerow(entry)
+        self.entries += 1
+        return self._human_readable(entry)
+
+    def write_flags(self, flags):
+        flags = vars(flags) # As dictionary.
+        arg_file = os.path.join(self.result_dir, 'options.toml')
+        with open(arg_file, 'wt') as f:
+            toml.dump(flags, f)
+
+    def _human_readable(self, entry):
+        abuffer = []
+        for key, value in zip(self.headers, entry):
+            abuffer.append("{key}: {value}".format(key=key, value=value))
+        return "; ".join(abuffer)
+
+
+    def close(self):
+        self.file.close()
+
