7f09ed722f12b2dc041bade4bd10974566b624e6
Kenneth Blomqvist <kekeblom@gmail.com>
2018-10-05 16:33:00 +0300

    set sensible default arguments

--- conv_gp/arguments.py
+++ conv_gp/arguments.py
@@ -12,23 +12,23 @@ def default_parser():
             help="What to call the experiment. Determines the directory where results are dumped.")
     parser.add_argument('--lr-decay-steps', type=int, default=100000,
             help="The program uses exponential learning rate decay with 0.1 decay every lr-decay-steps.")
-    parser.add_argument('--test-every', type=int, default=1000,
+    parser.add_argument('--test-every', type=int, default=50000,
             help="How often to evaluate the test accuracy. Unit optimization iterations.")
     parser.add_argument('--test-size', type=int, default=10000)
     parser.add_argument('--num-samples', type=int, default=10)
     parser.add_argument('--log-dir', type=str, default='results',
             help="Directory to write the results to.")
     parser.add_argument('--lr', type=float, default=0.01)
-    parser.add_argument('--batch-size', type=int, default=64,
+    parser.add_argument('--batch-size', type=int, default=32,
             help="Minibatch size to use in optimization.")
     parser.add_argument('--optimizer', type=str, default='Adam',
             help="Either Adam or NatGrad")
 
-    parser.add_argument('-M', type=str, default='128,128',
+    parser.add_argument('-M', type=str, default='384,384',
             help="How many inducing points to use at each layer.")
-    parser.add_argument('--feature-maps', type=str, default='1')
+    parser.add_argument('--feature-maps', type=str, default='10')
     parser.add_argument('--filter-sizes', type=str, default='5,5')
-    parser.add_argument('--strides', type=str, default='1,1')
+    parser.add_argument('--strides', type=str, default='2,1')
     parser.add_argument('--base-kernel', type=str, default='rbf')
     parser.add_argument('--white', action='store_true', default=False)
 
